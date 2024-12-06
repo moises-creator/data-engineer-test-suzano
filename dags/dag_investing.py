d90cd5532569507b35279b9c3db7847257c9ff19
moises-creator <moises.ximenes5@gmail.com>
2024-12-06 08:26:54 -0300

    finalizado

--- dags/dag_investing.py
+++ dags/dag_investing.py
@@ -18,14 +18,14 @@ def read_sql_file(file_path):
 
 def load_bloomberg_task(**kwargs):
     """Processa e carrega os dados do Bloomberg no banco de dados."""
-    # Obtém os dados do XCom
+    
     data = kwargs['ti'].xcom_pull(task_ids='scrape_bloomberg')
 
-    # Verifica se o dado é uma string e converte para JSON
+    
     if isinstance(data, str):
         data = json.loads(data)
 
-    # Prepara os dados como uma lista de tuplas para inserção em batch
+    
     rows = [
         (
             row['rowDateTimestamp'][:10],  # Date
@@ -38,27 +38,27 @@ def load_bloomberg_task(**kwargs):
         for row in data['data']
     ]
 
-    # Usa o hook para inserir os dados em batch
+    
     pg_hook = PostgresHook(postgres_conn_id=DB_CONNECTION_ID)
     pg_hook.insert_rows(
-        table="Bloomberg_Commodity_Index",  # Nome da tabela
-        rows=rows,                         # Dados para inserir
-        target_fields=["date", "close", "open", "high", "low", "volume"],  # Campos alvo
-        commit_every=1000                  # Divide em lotes de 1000
+        table="Bloomberg_Commodity_Index",  
+        rows=rows,                         
+        target_fields=["date", "close", "open", "high", "low", "volume"],  
+        commit_every=1000                  
     )
 
 
 
 def load_usd_cny_task(**kwargs):
     """Processa e carrega os dados de USD/CNY no banco de dados."""
-    # Obtém os dados do XCom
+    
     data = kwargs['ti'].xcom_pull(task_ids='scrape_usd_cny')
 
-    # Verifica se o dado é uma string e converte para JSON
+    
     if isinstance(data, str):
         data = json.loads(data)
 
-    # Prepara os dados como uma lista de tuplas para inserção em batch
+    
     rows = [
         (
             row['rowDateTimestamp'][:10],  # Date
@@ -71,27 +71,26 @@ def load_usd_cny_task(**kwargs):
         for row in data['data']
     ]
 
-    # Usa o hook para inserir os dados em batch
+    
     pg_hook = PostgresHook(postgres_conn_id=DB_CONNECTION_ID)
     pg_hook.insert_rows(
-        table="usd_cny",  # Nome da tabela
-        rows=rows,        # Dados para inserir
-        target_fields=["date", "close", "open", "high", "low", "volume"],  # Campos alvo
-        commit_every=1000  # Divide em lotes de 1000
+        table="usd_cny",  
+        rows=rows,        
+        target_fields=["date", "close", "open", "high", "low", "volume"],  
+        commit_every=1000  
     )
-
 
 
 def load_china_index_task(**kwargs):
     """Processa e carrega os dados do índice da China no banco de dados."""
-    # Obtém os dados do XCom
+    
     data = kwargs['ti'].xcom_pull(task_ids='scrape_china_index')
 
-    # Verifique se o dado é uma string e converta para JSON
+    
     if isinstance(data, str):
         data = json.loads(data)
 
-    # Prepara os dados como uma lista de tuplas para inserção em batch
+    
     rows = [
         (
             datetime.utcfromtimestamp(row['timestamp'] // 1000).strftime('%Y-%m-%d'),  # Date
@@ -102,7 +101,7 @@ def load_china_index_task(**kwargs):
         for row in data['attr']
     ]
 
-    # Usa o hook para inserir os dados em batch
+    
     pg_hook = PostgresHook(postgres_conn_id=DB_CONNECTION_ID)
     pg_hook.insert_rows(
         table="chinese_caixin_services_index",  # Nome da tabela
@@ -114,20 +113,20 @@ def load_china_index_task(**kwargs):
 
 @dag(start_date=days_ago(1), schedule=None, catchup=False, tags=["scraping", "postgres"])
 def scraping_to_postgres():
-    # Tarefas de scraping
+    
     scrape_bloomberg_task = PythonOperator(
         task_id='scrape_bloomberg',
-        python_callable=scrape_bloomberg,  # Retorna os dados diretamente
+        python_callable=scrape_bloomberg,  
     )
 
     scrape_usd_cny_task = PythonOperator(
         task_id='scrape_usd_cny',
-        python_callable=scrape_usd_cny,  # Retorna os dados diretamente
+        python_callable=scrape_usd_cny,  
     )
 
     scrape_china_index_task = PythonOperator(
         task_id='scrape_china_index',
-        python_callable=scrape_china_index,  # Retorna os dados diretamente
+        python_callable=scrape_china_index,  
     )
 
     cleanup_bloomberg_task = PostgresOperator(
@@ -148,18 +147,18 @@ def scraping_to_postgres():
         sql="DROP TABLE IF EXISTS chinese_caixin_services_index;"
     )
 
-    # Tarefa de criação de tabelas
+    
     create_tables_task = PostgresOperator(
         task_id="create_tables",
         postgres_conn_id=DB_CONNECTION_ID,
         sql=read_sql_file("include/sql/create_tables.sql"),
     )
 
-    # Tarefas de carregamento
+    
     load_bloomberg = PythonOperator(
         task_id="load_bloomberg",
         python_callable=load_bloomberg_task,
-        provide_context=True,  # Habilita o uso de XCom
+        provide_context=True,  
     )
 
     load_usd_cny = PythonOperator(
@@ -174,7 +173,7 @@ def scraping_to_postgres():
         provide_context=True,
     )
 
-    # Encadeamento das tarefas
+    
     chain(
         scrape_bloomberg_task,
         scrape_usd_cny_task,
