d1a738f06f0848f0d2d9fac049a5401d3bc9826f
moises-creator <moises.ximenes5@gmail.com>
2024-12-24 00:07:45 -0300

    building load bg

--- include/task_groups/load_bigquery.py
+++ include/task_groups/load_bigquery.py
@@ -1,8 +1,8 @@
 from asyncio import TaskGroup
-from airflow.providers.google.cloud.operators.bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 
 class LoadToBigQueryTG(TaskGroup):
-    def __init__(self, group_id, tooltip="Criacao da tabela arquivos na camada bronze do BigQuery e Cloud Storage", **kwargs):
+    def __init__(self, group_id, tooltip="Carregamento dos dados no BigQuery", **kwargs):
         super().__init__(group_id=group_id, tooltip=tooltip, **kwargs)
 
         
@@ -10,7 +10,7 @@ class LoadToBigQueryTG(TaskGroup):
             task_id="load_bloomberg",
             bucket="{{ params.bucket_name }}",
             source_objects=["bloomberg.csv"],
-            destination_project_dataset_table=f"{{params.project_id}}.{{params.dataset_id}}.bloomberg",
+            destination_project_dataset_table="{{params.project_id}}.{{params.dataset_id}}.bloomberg",
             source_format="CSV",
             skip_leading_rows=1,
             write_disposition="WRITE_TRUNCATE",
@@ -21,7 +21,7 @@ class LoadToBigQueryTG(TaskGroup):
             task_id="load_usd_cny",
             bucket="{{ params.bucket_name }}",
             source_objects=["usd_cny.csv"],
-            destination_project_dataset_table=f"{{params.project_id}}.{{params.dataset_id}}.usd_cny",
+            destination_project_dataset_table="{{params.project_id}}.{{params.dataset_id}}.usd_cny",
             source_format="CSV",
             skip_leading_rows=1,
             write_disposition="WRITE_TRUNCATE",
@@ -32,7 +32,7 @@ class LoadToBigQueryTG(TaskGroup):
             task_id="load_china_index",
             bucket="{{ params.bucket_name }}",
             source_objects=["china_index.csv"],
-            destination_project_dataset_table=f"{{params.project_id}}.{{params.dataset_id}}.chinese_caixin_services_index",
+            destination_project_dataset_table="{{params.project_id}}.{{params.dataset_id}}.chinese_caixin_services_index",
             source_format="CSV",
             skip_leading_rows=1,
             write_disposition="WRITE_TRUNCATE",
